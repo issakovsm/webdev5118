84d85223fc4a21514c88f010c7928ef205902fa1
issakovsm <141360542+issakovsm@users.noreply.github.com>
2023-09-14 23:39:57 +0600

    Create arrayreduce.js

---
+++ arrayreduce.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+  let reduced = init;
+  for (let i = 0; i < nums.length; i++) {
+    reduced = fn(reduced, nums[i]);
+  }
+  return reduced;
+};
